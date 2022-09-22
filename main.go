62950b45247cc9ca0541dc5274a1e95d6963c2af
Roberto Plaza Romero <roberto.plaza@gfk.com>
2022-09-22 18:01:27 +0200

    second commit

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
+var indent = flag.String("t", "", "indent character")
+
 func main() {
+	flag.Parse()
 	var data any
 	if err := yaml.NewDecoder(os.Stdin).Decode(&data); err != nil {
 		log.Fatalf("unable to unmarshal: %v", err)
 	}
-	if err := json.NewEncoder(os.Stdout).Encode(&data); err != nil {
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", *indent)
+	if err := enc.Encode(&data); err != nil {
 		log.Fatalf("unable to marshal: %v", err)
 	}
 }
